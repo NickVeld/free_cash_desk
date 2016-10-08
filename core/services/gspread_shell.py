f441b5332cf839842ace80cc645b5478b5ba764b
NickVeld <nickveld@users.noreply.github.com>
2016-10-09 01:29:05 +0300

    Change auth way

--- core/services/gspread_shell.py
+++ core/services/gspread_shell.py
@@ -1,19 +1,18 @@
 import json
-from gspread import Client as Client
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 class GShell:
     def __init__(self):
         self.ssheet = None
 
     def get_from_config(self, config):
-        f = open("FreeCashDesk.json", 'r')
-        client = Client(json.loads(f.read()))
-        f.close()
-        client.login()
-        self.ssheet = client.open_by_key(config["gspread_key"])
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name("FreeCashDesk.json", scope)
+        self.ssheet = gspread.authorize(credentials).open_by_key(config["gspread_key"])
 
     def send(self, sheet_name, row, column, info):
         sheet = self.ssheet.worksheet(sheet_name)
         for col in range(len(info)):
-            for r in range(min(3, len(col))):
+            for r in range(min(3, len(info[col]))):
                 sheet.update_cell(row + r, column + col, info[col][r])
