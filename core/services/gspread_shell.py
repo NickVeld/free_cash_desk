b53e1c07f5396f03c43e3d40ac4cf3088c97c2f6
NickVeld <nickveld@users.noreply.github.com>
2016-10-09 16:17:58 +0300

    Ability to send the table link

--- core/services/gspread_shell.py
+++ core/services/gspread_shell.py
@@ -5,14 +5,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 class GShell:
     def __init__(self):
         self.ssheet = None
+        self.gspread_key = ""
 
     def get_from_config(self, config):
+        self.gspread_key = config["gspread_key"]
         scope = ['https://spreadsheets.google.com/feeds']
         credentials = ServiceAccountCredentials.from_json_keyfile_name("FreeCashDesk.json", scope)
-        self.ssheet = gspread.authorize(credentials).open_by_key(config["gspread_key"])
+        self.ssheet = gspread.authorize(credentials).open_by_key(self.gspread_key)
 
     def send(self, address, info):
         sheet = self.ssheet.worksheet(address[0])
         for column in range(len(info)):
             for row in range(min(3, len(info[column]))):
                 sheet.update_cell(row + address[1], column + address[2], info[column][row])
+
+    def get_table(self):
+        return "https://docs.google.com/spreadsheets/d/" + self.gspread_key
